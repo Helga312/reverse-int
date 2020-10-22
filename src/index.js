b3dcc0671d8f28423b74b0fe87860a25e69e72ac
Helga312 <may.traum@gmail.com>
2020-10-22 15:34:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = n.toString().split(""),
+        extraSym = "";
+
+    +arr[0] ? extraSym = '' : arr.shift();
+
+    return `${extraSym}${arr.reverse().join("")}`;
 }
